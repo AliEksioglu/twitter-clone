0b375220ae593f910f18795762e1c8b610c35633
Ali <alieksioglu5306@gmail.com>
2023-09-29 14:17:15 +0300

    commit-4

--- src/layouts/sidebar/menu/index.jsx
+++ src/layouts/sidebar/menu/index.jsx
@@ -2,6 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 import classNames from "classnames";
 import mainMenu from "../../../utils/const";
 import Button from "../../../componenets/button";
+import More from "./more";
 
 function Menu() {
   return (
@@ -28,21 +29,9 @@ function Menu() {
           )}
         </NavLink>
       ))}
-      <button className="py-[3px] block group">
-        <div className="p-2.5 rounded-full inline-flex items-center gap-5 group-hover:bg-[#eff3f41a]">
-          <div className="w-[26px] h-[26px] relative ">
-            <svg viewBox="0 0 24 24" aria-hidden="true">
-              <path
-                fill="white"
-                d="M3.75 12c0-4.56 3.69-8.25 8.25-8.25s8.25 3.69 8.25 8.25-3.69 8.25-8.25 8.25S3.75 16.56 3.75 12zM12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-4.75 11.5c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25S6 11.31 6 12s.56 1.25 1.25 1.25zm9.5 0c.69 0 1.25-.56 1.25-1.25s-.56-1.25-1.25-1.25-1.25.56-1.25 1.25.56 1.25 1.25 1.25zM13.25 12c0 .69-.56 1.25-1.25 1.25s-1.25-.56-1.25-1.25.56-1.25 1.25-1.25 1.25.56 1.25 1.25z"
-              ></path>
-            </svg>
-          </div>
-          <div className="pr-4 text-xl">Daha Fazla</div>
-        </div>
-      </button>
-      <div className="py-4 w-[90%]" >
-      <Button size='large' > Gönder </Button>
+      <More />
+      <div className="py-4 w-[90%]">
+        <Button size="large"> Gönder </Button>
       </div>
     </nav>
   );
